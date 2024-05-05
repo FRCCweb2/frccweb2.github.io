4651ffa0cb227c0bc0c0fb8b6d3608d45c622efa
FRCCweb2 <168943983+FRCCweb2@users.noreply.github.com>
2024-05-04 19:36:39 -0600

    Add files via upload

---
+++ ml.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+
+Welcome <?php echo $_GET["name"]; ?><br>
+Your email address is: <?php echo $_GET["email"]; ?>
+<br>
+Your phone number is <?php echo $_GET["phone"]; ?>
+<br>
+Your additional comments are: <?php echo $_GET["info"]; ?>
+
+</body>
+</html>
